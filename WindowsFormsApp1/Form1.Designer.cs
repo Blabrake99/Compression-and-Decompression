2b8ff26eb94c734808e033d81a813dde4cdef5d1
Brandon LaBrake <56169332+Blabrake99@users.noreply.github.com>
2019-11-02 18:28:40 -0400

    first added files

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Read = new System.Windows.Forms.Button();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lvOut = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btn_Read
+            // 
+            this.btn_Read.Location = new System.Drawing.Point(10, 35);
+            this.btn_Read.Name = "btn_Read";
+            this.btn_Read.Size = new System.Drawing.Size(75, 23);
+            this.btn_Read.TabIndex = 2;
+            this.btn_Read.Text = "Compress";
+            this.btn_Read.UseVisualStyleBackColor = true;
+            this.btn_Read.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(115, 9);
+            this.btn_Open.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(25, 19);
+            this.btn_Open.TabIndex = 3;
+            this.btn_Open.Text = "...";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lvOut
+            // 
+            this.lvOut.Location = new System.Drawing.Point(145, 9);
+            this.lvOut.Name = "lvOut";
+            this.lvOut.Size = new System.Drawing.Size(200, 154);
+            this.lvOut.TabIndex = 5;
+            this.lvOut.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "DeCompress";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 355);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lvOut);
+            this.Controls.Add(this.btn_Open);
+            this.Controls.Add(this.btn_Read);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Read;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView lvOut;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.XPath;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<Char> Temp = new List<char>(),
+            character = new List<char>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if the file exists do the following
+            if (File.Exists(textBox1.Text))
+            {
+                //to count how many times the letter/number repeats
+                int count = 0;
+                string Final, text;
+                text = File.ReadAllText(textBox1.Text);
+                char currentChar = text[1];
+                //creates a new txt file 
+                using (StreamWriter file = new StreamWriter("temp1.txt"))
+                {
+                    //gets each char in the txt file and adds it to a string
+                    foreach (char c in text)
+                    {
+                        //if the prev char = the current char being added to the string 
+                        // the counter adds 1 
+                        if (currentChar == c)
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                           if ( count == 1)
+                            {
+                                Final = "" + currentChar + ",";
+                                //makes the prev char = the new char that was added
+                                currentChar = c;
+                                //resets the counter 
+                                count = 1;
+                                //writes to the file 
+                                file.Write(Final);
+                            }
+                            else
+                            {
+                                //what is being written to the txt file
+                                Final = "" + currentChar + count.ToString() + ",";
+                                //makes the prev char = the new char that was added
+                                currentChar = c;
+                                //resets the counter 
+                                count = 1;
+                                //writes to the file 
+                                file.Write(Final);
+                            }
+                        }
+                        character.Add(c);
+                    }
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //if file exhists 
+            if (File.Exists(textBox1.Text))
+            {
+                //writes to a new file
+                using (StreamWriter file = new StreamWriter("temp2.txt"))
+                {
+                    //reads all the text from the given text file and converts it to a string
+                    string text = File.ReadAllText(textBox1.Text);
+                    //splits everything between the ,
+                    string[] Textray = text.Split(',');
+                    //loops through everything in the Textray[]
+                    foreach (string s in Textray)
+                    {
+                        //checks if the length is bigger then 0
+                        if (s.Length > 0)
+                        {
+                            int num = 0;
+                            char character = s[0];
+                            string stringedNum = s.Substring(1, s.Length - 1);
+                            if (stringedNum.Length != 0)
+                            {
+                                //parses the ints in the string 
+                                 num = int.Parse(stringedNum);
+                            }
+                            else
+                            {
+                                num = 1;
+                            }
+                            //writes to the file 
+                            for (int i = 0; i < num; i++)
+                            {
+                                file.Write(character);
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse .txt Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "xml",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
